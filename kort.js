c7c23cd65801256ad32cc66696014d9e69ac0404
SiljeP <u2440913@rts-365.dk>
2023-03-10 10:51:57 +0100

    opgave interaktiv kort færdigt

---
+++ kort.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector("#DivKassePunkt1").addEventListener("mouseover",function(){
+        vispunkt1info();
+    })
+
+    document.querySelector("#DivKassePunkt1").addEventListener("mouseout", function(){
+        gempunkt1info();
+    })
+
+    document.querySelector("#DivKassePunkt2").addEventListener("mouseover", function(){
+        vispunkt2info();
+    } )
+
+    document.querySelector("#DivKassePunkt2").addEventListener("mouseout", function(){
+        gempunkt2info();
+    })
+
+})
+
+function vispunkt1info()
+{
+	document.querySelector("#PTag1").style.display = 'inline-block';
+}
+function gempunkt1info()
+{
+	document.querySelector("#PTag1").style.display = 'none';
+}
+
+function vispunkt2info()
+{
+	document.querySelector("#PTag2").style.display = 'inline-block';
+}
+function gempunkt2info()
+{
+	document.querySelector("#PTag2").style.display = 'none';
+}
